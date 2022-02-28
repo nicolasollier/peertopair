72d5b78257f126c9d1ef4dfae7b8b9894d046aa0
Fabien Cornen <fcornen@mandarine-gestion.com>
2022-02-28 17:21:36 +0100

    seed & controller event

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_02_28_152445) do
-=======
-ActiveRecord::Schema.define(version: 2022_02_28_151215) do
->>>>>>> 92cc980e0ea768c65b3879f3c6f574631d64b7e6
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+Format = %w(Remote Office)
+event_type = %w(Lunch Drink Coffee)
+status = %w(Passed Pending Current Coming)
+
+puts 'starting seed'
+Event.destroy_all
+
+puts 'previous seed destroyed'
+puts 'creating new seed'
+
+event1 = Event.new(start_date: ("2022-02-28 12-00"), end_date: ("2022-02-28 14-00"),
+format: Format.sample, event_type: event_type.sample, status: status.sample)
+event1.save!
+puts 'event1 done!'
+
+event2 = Event.new(start_date: ("2022-02-28 12-00"), end_date: ("2022-02-28 14-00"),
+format: Format.sample, event_type: event_type.sample, status: status.sample)
+event2.save!
+puts 'event2 done!'
+
+event3 = Event.new(start_date: ("2022-02-28 12-00"), end_date: ("2022-02-28 14-00"),
+format: Format.sample, event_type: event_type.sample, status: status.sample)
+event3.save!
+puts 'event3 done!'
+
+event4 = Event.new(start_date: ("2022-02-28 12-00"), end_date: ("2022-02-28 14-00"),
+format: Format.sample, event_type: event_type.sample, status: status.sample)
+event4.save!
+puts 'event4 done!'
+
+event5 = Event.new(start_date: ("2022-02-28 12-00"), end_date: ("2022-02-28 14-00"),
+format: Format.sample, event_type: event_type.sample, status: status.sample)
+event5.save!
+puts 'event5 done!'
+
+event6 = Event.new(start_date: ("2022-02-28 12-00"), end_date: ("2022-02-28 14-00"),
+format: Format.sample, event_type: event_type.sample, status: status.sample)
+event6.save!
+puts 'event6 done!'
