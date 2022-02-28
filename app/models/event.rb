b7de6fa402a521a2fcdbd919bacc8d212af865a9
Vincent Sengel <vincent.sengel@gmail.com>
2022-02-28 16:34:11 +0100

    add references to user_event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :users_events
+  has_many :user, through: :users_events
   validates :start_date, :end_date, presence: true
   validates :format, inclusion: { in: %w(Remote Office) }
   validates :event_type, inclusion: { in: %w(Lunch Drink Coffee) }
