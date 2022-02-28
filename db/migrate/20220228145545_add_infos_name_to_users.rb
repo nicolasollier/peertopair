28d774fd8833e244a334c49652271cf4b50b4006
Vincent Sengel <vincent.sengel@gmail.com>
2022-02-28 15:59:02 +0100

    created migration for first_name, last_name, dept, job_title

---
+++ db/migrate/20220228145545_add_infos_name_to_users.rb
@@ -0,0 +1,8 @@
+class AddInfosNameToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :department, :string
+    add_column :users, :job_title, :string
+  end
+end
