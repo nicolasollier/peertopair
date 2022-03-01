2ba425f052529ee94ee6e0d4230939f27487a3fe
Fabien Cornen <fcornen@mandarine-gestion.com>
2022-03-01 12:25:44 +0100

    modif again

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,13 @@
 class EventsController < ApplicationController
+  # before_action :set_event
+
   def index
     @events = Event.all
   end
+
+  private
+
+  # def set_event
+  #   @event = Event.find(params[:id])
+  # end
 end
