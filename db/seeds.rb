dabab05f6c66391d21f27a25c76a7b7e691a3407
Fabien Cornen <fcornen@mandarine-gestion.com>
2022-03-03 16:41:13 +0100

    fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ user3 = User.new(email: "ghi@gmail.com", password: "123456", first_name: "Stepha
 user3.save!
 puts 'user3 done!'
 
-event1 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event1 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: "Lunch", status: "Pending")
 event1.save!
 puts 'event1 done!'
@@ -33,7 +33,7 @@ user_event1 = UserEvent.new(user: user1, event: event1, ranking_done: false)
 user_event1.save!
 puts 'user_event1 done!'
 
-event2 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event2 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: "Lunch", status: "Pending")
 event2.save!
 puts 'event2 done!'
@@ -42,7 +42,7 @@ user_event2 = UserEvent.new(user: user1, event: event2, ranking_done: false)
 user_event2.save!
 puts 'user_event2 done!'
 
-event3 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event3 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: "Lunch", status: "Pending")
 event3.save!
 puts 'event3 done!'
@@ -51,7 +51,7 @@ user_event3 = UserEvent.new(user: user1, event: event3, ranking_done: false)
 user_event3.save!
 puts 'user_event3 done!'
 
-event4 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event4 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: event_type.sample, status: "Pending")
 event4.save!
 puts 'event4 done!'
@@ -60,7 +60,7 @@ user_event4 = UserEvent.new(user: user2, event: event4, ranking_done: false)
 user_event4.save!
 puts 'user_event4 done!'
 
-event5 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event5 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: event_type.sample, status: "Pending")
 event5.save!
 puts 'event5 done!'
@@ -69,7 +69,7 @@ user_event5 = UserEvent.new(user: user3, event: event5, ranking_done: false)
 user_event5.save!
 puts 'user_event5 done!'
 
-event6 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event6 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: event_type.sample, status: "Pending")
 event6.save!
 puts 'event6 done!'
@@ -78,7 +78,7 @@ user_event6 = UserEvent.new(user: user3, event: event6, ranking_done: false)
 user_event6.save!
 puts 'user_event6 done!'
 
-event7 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event7 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: event_type.sample, status: status.sample)
 event7.save!
 puts 'event7 done!'
@@ -87,7 +87,7 @@ user_event7 = UserEvent.new(user: user1, event: event7, ranking_done: false)
 user_event7.save!
 puts 'user_event7 done!'
 
-event8 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event8 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: event_type.sample, status: status.sample)
 event8.save!
 puts 'event8 done!'
@@ -96,7 +96,7 @@ user_event8 = UserEvent.new(user: user1, event: event8, ranking_done: false)
 user_event8.save!
 puts 'user_event8 done!'
 
-event9 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event9 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: event_type.sample, status: status.sample)
 event9.save!
 puts 'event9 done!'
@@ -105,7 +105,7 @@ user_event9 = UserEvent.new(user: user2, event: event9, ranking_done: false)
 user_event9.save!
 puts 'user_event9 done!'
 
-event10 = Event.new(start_date: ("2022-02-28 12:00:00"), end_date: ("2022-02-28 14:00:00"),
+event10 = Event.new(start_date: ("2022-03-04 12:00:00"), end_date: ("2022-03-04 14:00:00"),
 format: Format.sample, event_type: event_type.sample, status: status.sample)
 event10.save!
 puts 'event10 done!'
