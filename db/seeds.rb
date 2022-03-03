f46443c15d68931f5680acb4ce6d9c6ad3402792
Fabien Cornen <fcornen@mandarine-gestion.com>
2022-03-03 12:39:28 +0100

    ranking done ok quand on submit le ranking

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ format: Format.sample, event_type: "Lunch", status: "Pending")
 event1.save!
 puts 'event1 done!'
 
-user_event1 = UserEvent.new(user: user1, event: event1)
+user_event1 = UserEvent.new(user: user1, event: event1, ranking_done: false)
 user_event1.save!
 puts 'user_event1 done!'
 
@@ -38,7 +38,7 @@ format: Format.sample, event_type: "Lunch", status: "Pending")
 event2.save!
 puts 'event2 done!'
 
-user_event2 = UserEvent.new(user: user1, event: event2)
+user_event2 = UserEvent.new(user: user1, event: event2, ranking_done: false)
 user_event2.save!
 puts 'user_event2 done!'
 
@@ -47,7 +47,7 @@ format: Format.sample, event_type: "Lunch", status: "Pending")
 event3.save!
 puts 'event3 done!'
 
-user_event3 = UserEvent.new(user: user1, event: event3)
+user_event3 = UserEvent.new(user: user1, event: event3, ranking_done: false)
 user_event3.save!
 puts 'user_event3 done!'
 
@@ -56,7 +56,7 @@ format: Format.sample, event_type: event_type.sample, status: "Pending")
 event4.save!
 puts 'event4 done!'
 
-user_event4 = UserEvent.new(user: user2, event: event4)
+user_event4 = UserEvent.new(user: user2, event: event4, ranking_done: false)
 user_event4.save!
 puts 'user_event4 done!'
 
@@ -65,7 +65,7 @@ format: Format.sample, event_type: event_type.sample, status: "Pending")
 event5.save!
 puts 'event5 done!'
 
-user_event5 = UserEvent.new(user: user3, event: event5)
+user_event5 = UserEvent.new(user: user3, event: event5, ranking_done: false)
 user_event5.save!
 puts 'user_event5 done!'
 
@@ -74,7 +74,7 @@ format: Format.sample, event_type: event_type.sample, status: "Pending")
 event6.save!
 puts 'event6 done!'
 
-user_event6 = UserEvent.new(user: user3, event: event6)
+user_event6 = UserEvent.new(user: user3, event: event6, ranking_done: false)
 user_event6.save!
 puts 'user_event6 done!'
 
@@ -83,7 +83,7 @@ format: Format.sample, event_type: event_type.sample, status: status.sample)
 event7.save!
 puts 'event7 done!'
 
-user_event7 = UserEvent.new(user: user1, event: event7)
+user_event7 = UserEvent.new(user: user1, event: event7, ranking_done: false)
 user_event7.save!
 puts 'user_event7 done!'
 
@@ -92,7 +92,7 @@ format: Format.sample, event_type: event_type.sample, status: status.sample)
 event8.save!
 puts 'event8 done!'
 
-user_event8 = UserEvent.new(user: user1, event: event8)
+user_event8 = UserEvent.new(user: user1, event: event8, ranking_done: false)
 user_event8.save!
 puts 'user_event8 done!'
 
@@ -101,7 +101,7 @@ format: Format.sample, event_type: event_type.sample, status: status.sample)
 event9.save!
 puts 'event9 done!'
 
-user_event9 = UserEvent.new(user: user2, event: event9)
+user_event9 = UserEvent.new(user: user2, event: event9, ranking_done: false)
 user_event9.save!
 puts 'user_event9 done!'
 
@@ -110,6 +110,6 @@ format: Format.sample, event_type: event_type.sample, status: status.sample)
 event10.save!
 puts 'event10 done!'
 
-user_event10 = UserEvent.new(user: user3, event: event10)
+user_event10 = UserEvent.new(user: user3, event: event10, ranking_done: false)
 user_event10.save!
 puts 'user_event10 done!'
