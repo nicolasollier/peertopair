59364f43993a930703100c29b28cab68ec38d2d0
Fabien Cornen <fcornen@mandarine-gestion.com>
2022-03-04 12:14:39 +0100

    map add geocoding sur event et modification de la seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_125401) do
+ActiveRecord::Schema.define(version: 2022_03_04_100825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2022_03_03_125401) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "canceled"
     t.string "status"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
   end
 
   create_table "user_events", force: :cascade do |t|
