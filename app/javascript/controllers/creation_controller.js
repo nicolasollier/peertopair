e4ae49780339d327ff0a9290fa385caaf95aacf1
Vincent Sengel <vincent.sengel@gmail.com>
2022-03-06 00:33:55 +0100

    not working, maybe should do vanilla js or fix it otherwise

--- app/javascript/controllers/creation_controller.js
+++ app/javascript/controllers/creation_controller.js
@@ -1,66 +1,156 @@
 import { Controller } from "@hotwired/stimulus"
+const when = document.querySelectorAll(".when")
+const month = document.querySelectorAll(".month")
+const week = document.querySelectorAll(".week")
+const day = document.querySelectorAll(".day")
+let deltaDay = 0;
 
-export default class extends Controller {
 
   connect() {
-    const when = document.querySelectorAll(".when")
 
     when.forEach((elt) => {
       this.when(elt)
-
     })
-
-    const month = document.querySelectorAll(".month")
 
     month.forEach((elt) => {
       this.month(elt)
+    })
 
+    week.forEach((elt) => {
+      this.week(elt)
+    })
+
+    day.forEach((elt) => {
+      this.day(elt)
     })
 
   };
 
   when(elt) {
+    console.log('when')
 
     elt.addEventListener("click", (event) => {
-      // do something on click of an element
+
       const screen_1 = event.target.value
-      console.log(screen_1)
 
-      if (screen_1 === 'today') {
-        const date = 1
-        console.log("go to screen 3 !")
+      switch (screen_1) {
+        case 'today':
+          deltaDay = 0
+          this.onOff(elt,when, day)
+          break;
 
-      } else if (screen_1 === 'week') {
-        console.log("go to screen 2 !")
+        case 'week':
+          this.onOff(elt,when, week)
+          break;
 
-      } else {
-        console.log("go to screen 1 !")
+        case 'month':
+          this.onOff(elt,when, month)
+          break;
 
       }
     })
-
   }
 
   month(elt) {
 
     elt.addEventListener("click", (event) => {
-      // do something on click of an element
-      const screen_1 = event.target.value
-      console.log(screen_1)
+      console.log('month')
 
-      if (screen_1 === 'today') {
-        const date = 1
-        console.log("go to screen 3 !")
+      const screen_2 = elt.value
 
-      } else if (screen_1 === 'week') {
-        console.log("go to screen 2 !")
+      switch (screen_2) {
+        case 'week_2':
+          console.log(deltaDay)
+          deltaDay += 7
+          console.log(deltaDay)
+          break;
 
-      } else {
-        console.log("go to screen 1 !")
+        case 'week_3':
+          console.log(deltaDay)
+          deltaDay += 14
+          console.log(deltaDay)
+          break;
 
+        case 'week_4':
+          console.log(deltaDay)
+          deltaDay += 21
+          console.log(deltaDay)
+          break;
+      }
+
+      this.onOff(elt,month, week)
+    })
+  }
+
+  week(elt) {
+
+    elt.addEventListener("click", (event) => {
+      const today = new Date().getDay();
+      const screen_3 = elt
+      let delta = 0
+      console.log(screen_3)
+
+      switch (screen_3) {
+        case 'mon':
+          delta = 1
+        break;
+
+        case 'tue':
+          delta = 2
+          break;
+
+        case 'wed':
+          delta = 3
+        break;
+
+        case 'thu':
+          delta = 4
+        break;
+
+        case 'fri':
+          delta = 5
+        break;
+      }
+
+      console.log(deltaDay)
+      deltaDay += delta - today
+      console.log(deltaDay)
+      this.onOff(elt,week, day)
+    })
+  }
+
+  day(elt) {
+
+    elt.addEventListener("click", (event) => {
+      console.log(deltaDay)
+      const screen_4 = elt.value
+
+      switch (screen_4) {
+        case 'today':
+          this.onOff(elt,when, day)
+          break;
+
+        case 'week':
+          this.onOff(elt,when, week)
+          break;
+
+        case 'month':
+          this.onOff(elt,when, month)
+          break;
+
+        default:
+          break;
       }
     })
+  }
+
+
+  onOff(elt, commingFrom, goingTo) {
+
+    commingFrom.forEach((elt) => elt.style.display = "none")
+    goingTo.forEach((elt) => elt.style.display = "")
 
   }
+export default class extends Controller {
 
 }
