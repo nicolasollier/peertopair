95b4cf87656cf1700037dd9a52de799262979472
Vincent Sengel <vincent.sengel@gmail.com>
2022-03-09 11:07:13 +0100

    first functional version

--- app/services/get_restaurants.rb
+++ app/services/get_restaurants.rb
@@ -5,7 +5,6 @@ class GetRestaurants
   end
 
   def call
-    return unless @event.event_type
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@user.latitude},#{@user.longitude}&radius=200&type=restaurant&key=#{ENV['GOOGLE_TOKEN']}"
     user_serialized = URI.open(url).read
     @response = JSON.parse(user_serialized)
