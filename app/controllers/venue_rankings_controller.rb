378e672e1931190c0e9b3408b9f740048ebfc969
Vincent Sengel <vincent.sengel@gmail.com>
2022-03-09 17:28:52 +0100

    fonctionnel

--- app/controllers/venue_rankings_controller.rb
+++ app/controllers/venue_rankings_controller.rb
@@ -6,7 +6,6 @@ class VenueRankingsController < ApplicationController
     # @response = GetRestaurants.new(current_user, @event).call
     if (UserEvent.where(event: @event.id).count) == 2
       @venues = VenueRanking.select("name").where(event: @event)
-      raise
     end
   end
 
@@ -15,11 +14,21 @@ class VenueRankingsController < ApplicationController
   end
 
   def create
+    @venueranking = VenueRanking.new(venue_ranking_params)
 
-    if params.user_id
-
-      raise
-
+    if @venueranking.valid?
+      i = 1
+      n = 1
+      params.dig(:venue_rankings, :ranking).gsub(/["\"]/,'').split(",").each do |rest|
+      @venueranking = VenueRanking.new(venue_ranking_params)
+      @venueranking.rank = i
+      @venueranking.place_name = rest
+      @venueranking.note = n.round(2)
+      @venueranking.save!
+      i += 1
+      n -= 0.20
+      end
+      UserEvent.create(user_id: current_user.id , event_id: @venueranking.event.id)
     else
       @event = current_user.events.last
       i = 1
@@ -37,23 +46,30 @@ class VenueRankingsController < ApplicationController
         n -= 0.20
       end
     end
-
-    update()
-
+    update(@venueranking.event)
   end
 
-  def update
+  def update(event)
     #Si ils sont deux dans l'user event et qu'il rentre ici c'est qu'il a submit son formulaire
     # Dans ce cas on récupère le best compromise des deux
-    if (UserEvent.where(event: @event.id).count) == 2
-      @venues = VenueRanking.select("place_name, sum(note) as note").where(event: @event.id).where(user: current_user).group("place_name")
-      @max_note = @venues.sort_by { |venue| venue.note}.last
-      @event.venue = @max_note.place_name
-      address = Venue.select("address").find_by(name: @max_note.place_name)
-      @event.address = address.address
-      @event.save!
-    else
+    if (event.users.count) == 2
+    # if (UserEvent.where(event: event.id).count) == 2
+    @venues = VenueRanking.select("place_name, sum(note) as note").where(event: event.id).group("place_name")
 
+    @max_note = @venues.sort_by { |venue| venue.note}.last
+    event.venue = @max_note.place_name
+    address = Venue.select("address").find_by(name: @max_note.place_name)
+    event.address = address.address
+    event.save!
+    else
+      redirect_to dashboard_path
     end
   end
+
+  private
+
+  def venue_ranking_params
+    params.require(:venue_rankings).permit(:rank, :event_id, :user_id )
+  end
+
 end
