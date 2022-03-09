3315bb770adccad790a8653824defb9aa58b9a5b
Nicolas Ollier <nicolas.f.ollier@gmail.com>
2022-03-09 16:11:59 +0100

    new icons

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -9,7 +9,6 @@ export default class extends Controller {
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-    console.log(this.apiKeyValue)
     this.map = new mapboxgl.Map({
       container: this.element,
       width: '900px',
