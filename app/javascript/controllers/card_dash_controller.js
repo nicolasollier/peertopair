b39ec1c04525125557eb4a9b614a1a9349a62d1f
stephanaltmuller <stephan.altmuller@gmail.com>
2022-03-10 09:50:03 +0100

    edit cards 2 ok

--- app/javascript/controllers/card_dash_controller.js
+++ app/javascript/controllers/card_dash_controller.js
@@ -3,24 +3,30 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "unfold", "chevron" ]
   connect() {
+    console.log(this.chevronTarget);
   };
 
   unfold() {
     const everything = document.querySelectorAll(".fold");
     const somethings = this.unfoldTarget.classList;
 
+    // quand l'action est triggered
+    // sélectionner l'ensemble des éléments et les 'replier'
+    // this.elements à déplier
+
+
+    console.log(this.chevronTarget);
+    this.chevronTarget.classList.toggle('rotate');
+
     if (somethings.contains("hide")) {
       everything.forEach((item) => {
         item.classList.add("hide");
       })
       this.unfoldTarget.classList.remove("hide");
-      this.chevronTarget.classList.toggle("d-none");
 
     } else {
 
       this.unfoldTarget.classList.add("hide");
-      this.chevronTarget.classList.toggle("d-none");
-
     }
   }
 
