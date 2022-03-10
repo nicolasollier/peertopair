e2d876341c3406acc93deb2c644729358c7d82de
Fabien Cornen <fcornen@mandarine-gestion.com>
2022-03-10 09:05:42 +0100

    map qui tourne

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -9,18 +9,30 @@ export default class extends Controller {
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-    this.map = new mapboxgl.Map({
+    const map = this.map = new mapboxgl.Map({
       container: this.element,
       width: '900px',
       height: '900px',
       style: "mapbox://styles/mapbox/streets-v10"
     })
-    
+
+    function rotateCamera(timestamp) {
+      // clamp the rotation between 0 -360 degrees
+      // Divide timestamp by 100 to slow rotation to ~10 degrees / sec
+      map.rotateTo((timestamp / 600) % 360, { duration: 0 });
+      // Request the next frame of the animation.
+      requestAnimationFrame(rotateCamera);
+    }
+
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+    rotateCamera(0);
+
     this.element.parentNode.classList.add("fold")
     this.element.parentNode.classList.add("hide")
   }
+
+
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
@@ -34,6 +46,7 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 14, duration: 0})
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 14, duration: 0,pitch: 70, bearing:100})
   }
+
 }
