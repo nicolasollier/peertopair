948efcf5730e017afdbac5f266d68fb82a20442e
stephanaltmuller <stephan.altmuller@gmail.com>
2022-03-10 10:32:51 +0100

    front card now wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,19 +11,18 @@ User.destroy_all
 puts 'previous seed destroyed'
 puts 'creating new seed'
 
+# user1 = User.new(email: "nicolas@renault.com", password: "123456", first_name: "Nicolas", last_name: "Ollier", address: "5 rue de Menilmontant", department: "IT", job_title: "Chief Technical Officer")
+# photo1 = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQHtPYPXOcW6_A/profile-displayphoto-shrink_800_800/0/1563874170099?e=1652313600&v=beta&t=EfQGdWFw_X7rKBEAPZIx2k62BvsSGTkNXUru8O0Viwk')
+# user1.photo.attach(io: photo1, filename: "#{user1.first_name}.jpg")
 
-user1 = User.new(email: "nicolas@renault.com", password: "123456", first_name: "Nicolas", last_name: "Ollier", address: "5 rue de Menilmontant", department: "IT", job_title: "Chief Technical Officer")
-photo1 = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQHtPYPXOcW6_A/profile-displayphoto-shrink_800_800/0/1563874170099?e=1652313600&v=beta&t=EfQGdWFw_X7rKBEAPZIx2k62BvsSGTkNXUru8O0Viwk')
-user1.photo.attach(io: photo1, filename: "#{user1.first_name}.jpg")
+# user1.save!
+# puts 'user1 done!'
 
-user1.save!
-puts 'user1 done!'
-
-user2 = User.new(email: "fabien@renault.com", password: "123456", first_name: "Fabien", last_name: "Cornen", address: "40 avenue george V", department: "Legal", job_title: "Chief Legal Officer")
-photo2 = URI.open('https://media-exp1.licdn.com/dms/image/C4D03AQG5k8_iHRknIw/profile-displayphoto-shrink_800_800/0/1600451248024?e=1652313600&v=beta&t=42u0m5UwhQ7iKNAVR3ctD7DHwcKDkDUUqq9dXbniEAA')
-user2.photo.attach(io: photo2, filename: "#{user2.first_name}.jpg")
-user2.save!
-puts 'user2 done!'
+# user2 = User.new(email: "fabien@renault.com", password: "123456", first_name: "Fabien", last_name: "Cornen", address: "40 avenue george V", department: "Legal", job_title: "Chief Legal Officer")
+# photo2 = URI.open('https://media-exp1.licdn.com/dms/image/C4D03AQG5k8_iHRknIw/profile-displayphoto-shrink_800_800/0/1600451248024?e=1652313600&v=beta&t=42u0m5UwhQ7iKNAVR3ctD7DHwcKDkDUUqq9dXbniEAA')
+# user2.photo.attach(io: photo2, filename: "#{user2.first_name}.jpg")
+# user2.save!
+# puts 'user2 done!'
 
 user3 = User.new(email: "stephan@renault.com", password: "123456", first_name: "Stephan", last_name: "Altmuller", address: "8 rue des nanettes", department: "Marketing", job_title: "Digital Project Manager")
 photo3 = URI.open('https://media-exp1.licdn.com/dms/image/C4D03AQHyWQyqHpm6fw/profile-displayphoto-shrink_200_200/0/1618685738223?e=1648684800&v=beta&t=vDARt-87A84n0cmdqKPstcerBeGzRLCN5KZOU_qVWFY')
@@ -37,40 +36,37 @@ user4.photo.attach(io: photo4, filename: "#{user4.first_name}.jpg")
 user4.save!
 puts 'user4 done!'
 
-user5 = User.new(email: "thierry@renault.com", password: "123456", first_name: "Thierry", last_name: "Pieton", address: "8 rue des nanettes", department: "Finance", job_title: "Chief Financial Officer")
-photo5 = URI.open('https://www.daf-mag.fr/Assets/Img/MOUVEMENT/2022/3/369932/Thierry-Pieton-promu-directeur-financier-groupe-Renault-F.jpg')
-user5.photo.attach(io: photo5, filename: "#{user5.first_name}.jpg")
-user5.save!
-puts 'user5 done!'
+# user5 = User.new(email: "thierry@renault.com", password: "123456", first_name: "Thierry", last_name: "Pieton", address: "8 rue des nanettes", department: "Finance", job_title: "Chief Financial Officer")
+# photo5 = URI.open('https://www.daf-mag.fr/Assets/Img/MOUVEMENT/2022/3/369932/Thierry-Pieton-promu-directeur-financier-groupe-Renault-F.jpg')
+# user5.photo.attach(io: photo5, filename: "#{user5.first_name}.jpg")
+# user5.save!
+# puts 'user5 done!'
 
-user6 = User.new(email: "xavier@renault.com", password: "123456", first_name: "Xavier", last_name: "Lançon", address: "8 rue des nanettes", department: "Marketing", job_title: "Head of Digital Media")
-photo6 = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQFj5obxIHqFBw/profile-displayphoto-shrink_800_800/0/1517631403544?e=1652313600&v=beta&t=Dflqj6xwQjBKvK7CYdvnyIGMdHkHhyNkxrp24Jf14kI')
-user6.photo.attach(io: photo6, filename: "#{user6.first_name}.jpg")
-user6.save!
-puts 'user6 done!'
+# user6 = User.new(email: "xavier@renault.com", password: "123456", first_name: "Xavier", last_name: "Lançon", address: "8 rue des nanettes", department: "Marketing", job_title: "Head of Digital Media")
+# photo6 = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQFj5obxIHqFBw/profile-displayphoto-shrink_800_800/0/1517631403544?e=1652313600&v=beta&t=Dflqj6xwQjBKvK7CYdvnyIGMdHkHhyNkxrp24Jf14kI')
+# user6.photo.attach(io: photo6, filename: "#{user6.first_name}.jpg")
+# user6.save!
+# puts 'user6 done!'
 
-user7 = User.new(email: "vincent@renault.com", password: "123456", first_name: "Vincent", last_name: "Sengel", address: "8 rue des nanettes", department: "IT", job_title: "Chief Information Officer")
-photo7 = URI.open('https://media.gqmagazine.fr/photos/5f1fe7820bc2e326afb66dba/master/w_3000,h_2000,c_limit/2019-brad-pitt-ad-astra.jpg')
-user7.photo.attach(io: photo7, filename: "#{user7.first_name}.jpg")
-user7.save!
-puts 'user7 done!'
-
+# user7 = User.new(email: "vincent@renault.com", password: "123456", first_name: "Vincent", last_name: "Sengel", address: "8 rue des nanettes", department: "IT", job_title: "Chief Information Officer")
+# photo7 = URI.open('https://media.gqmagazine.fr/photos/5f1fe7820bc2e326afb66dba/master/w_3000,h_2000,c_limit/2019-brad-pitt-ad-astra.jpg')
+# user7.photo.attach(io: photo7, filename: "#{user7.first_name}.jpg")
+# user7.save!
+# puts 'user7 done!'
 
 # #######Vincent start
 
+event1 = Event.new(start_date: ("2022-03-10 11:00:00"), end_date: ("2022-03-10 12:00:00"), format: "Office", event_type: "Coffee", canceled: false)
+event1.save!
+puts 'event1 done!'
 
-# event1 = Event.new(start_date: ("2022-03-09 16:00:00"), end_date: ("2022-03-09 16:30:00"), format: "Office", event_type: "Coffee", canceled: false)
-# event1.save!
-# puts 'event1 done!'
+user_event1 = UserEvent.new(user: user3, event: event1, ranking_done: false)
+user_event1.save!
+puts 'user_event1 done!'
 
-# user_event1 = UserEvent.new(user: user3, event: event1, ranking_done: false)
-# user_event1.save!
-# puts 'user_event1 done!'
-
-# user_event2 = UserEvent.new(user: user4, event: event1, ranking_done: false)
-# user_event2.save!
-# puts 'user_event2 done!'
-
+user_event2 = UserEvent.new(user: user4, event: event1, ranking_done: false)
+user_event2.save!
+puts 'user_event2 done!'
 
 # event2 = Event.new(start_date: ("2022-03-11 16:00:00"), end_date: ("2022-03-11 16:30:00"), format: "Office", event_type: "Coffee", canceled: false)
 # event2.save!
